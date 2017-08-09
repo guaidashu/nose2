3e6131261c6ed7400965213cd69e3591c8e3e821
怪大叔丶 <1023767856@qq.com>
2017-08-09 23:59:03 +0800

    ok

--- public/js/project.js
+++ public/js/project.js
@@ -4,7 +4,7 @@
 		var self=this;
 		this.body=$(document.body);
 		// test用来弹出屏幕宽度用于自适应开发
-		this.test();
+		// this.test();
 
 		this.projectContainerAuto();
 		window.onresize=function(){
@@ -21,13 +21,29 @@
 		{
 			var self=this;
 			var screenWidth=parseInt($(document).width());
-			var height=parseInt($(".project1_right").height());
-			var height_2=parseInt($(".project1_left").height());
-			if(screenWidth > 650){
-				$(".project1").css("height",height+45+"px");
-			}else{
-				$(".project1").css("height",height+45+height_2+"px");
+			// 获取到project1的所有对象
+			var projectObj=document.getElementsByName("project1");
+			var obj2=document.getElementsByName("project1_left");
+			var obj3=document.getElementsByName("project1_right");
+			var len=projectObj.length;
+			var height;
+			for(var i=0;i<len;i++){
+				height2=obj2[i].offsetHeight;
+				height3=obj3[i].offsetHeight;
+				if(screenWidth > 650){
+					projectObj[i].style.height=height3+"px";
+				}else{
+					projectObj[i].style.height=height2+height3+"px";
+				}
 			}
+			// yy_init(height);
+			// var height=parseInt($(".project1_right").height());
+			// var height_2=parseInt($(".project1_left").height());
+			// if(screenWidth > 650){
+			// 	$(".project1").css("height",height+45+"px");
+			// }else{
+			// 	$(".project1").css("height",height+45+height_2+"px");
+			// }
 		}
 	}
 	window['project_function']=project_function;
--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -20,45 +20,45 @@
 
 <!--新改的部分-->
 <div class="ca_about_container">
-	<div class="project1">
-		<div class="project1_left">
-			<a href="http://sh.wyysdsa.cn/" target="_blank"><img class="img1" src="{{URL::asset('images/project/project4.png')}}"/></a>
-		</div>
-		<div class="project1_right">
-			<center><a href="http://sh.wyysdsa.cn/" target="_blank"><h1>Super Homework</h1></a></center>
+	<div class="project1" name="project1">
+		<div class="project1_right" name="project1_right">
+			<a href="http://sh.wyysdsa.cn/" class="project_a" target="_blank"><h1>Super Homework</h1></a>
 			Super Homework是一个可以发布作业，在线答疑，答题监控，在线执行源代码的网页。<br/>
 			通过绑定学校的教务系统，进准定位方便快捷接受作业信息，老师根据班级发布各类作业、
 			在线批改学生作业完成情况Excel导出。
 		</div>
-	</div>
-	<div class="project1">
-		<div class="project1_left">
-			<a href="http://www.wyysdsa.com/stucontrol/" target="_blank"><img class="img2" src="{{URL::asset('images/project/project5.png')}}"/></a>
+		<div class="project1_left" name="project1_left">
+			<a href="http://sh.wyysdsa.cn/" target="_blank"><img class="img1" src="{{URL::asset('images/project/project4.png')}}"/></a>
 		</div>
-		<div class="project1_right">
-			<center><a href="http://www.wyysdsa.com/stucontrol/" target="_blank">
+	</div>
+	<div class="project1" name="project1">
+		<div class="project1_right" name="project1_right">
+			<a href="http://www.wyysdsa.com/stucontrol/" class="project_a" target="_blank">
 			<p style="font-size:22px;letter-spacing:5px;font-weight:bold;">
 			查寝系统
-			</p></a></center>
+			</p></a>
 			查寝系统是一个可以在线进行查寝，宿舍保修和发布通知的系统。<br/>
 			老师可以在系统中发布通知，查寝。学生可以在系统中登记各类宿舍问题，及时接受老师通知，
 			发表各类留言。
 		</div>
-	</div>
-	<div class="project1">
-		<div class="project1_left">
-			<a href="http://www.wyysdsa.cn:1965/" target="_blank"><img class="img3" src="{{URL::asset('images/project/project6.png')}}"/></a>
+		<div class="project1_left" name="project1_left">
+			<a href="http://www.wyysdsa.com/stucontrol/" target="_blank"><img class="img2" src="{{URL::asset('images/project/project5.png')}}"/></a>
 		</div>
-		<div class="project1_right">
-			<center><a href="http://www.wyysdsa.cn:1965/" target="_blank">
-			<p style="font-size:22px;letter-spacing:5px;font-weight:bold;">川理在线</p></a></center>
+	</div>
+	<div class="project1" name="project1">
+		<div class="project1_right" name="project1_right">
+			<a href="http://www.wyysdsa.cn:1965/" class="project_a" target="_blank">
+			<p style="font-size:22px;letter-spacing:5px;font-weight:bold;">川理在线</p></a>
 			川理在线是一个登录学校教务系统，查询图书馆借阅、校园卡、英语四六级、学生缴费信息的平台。<br>
 			学生可在此平台中查询各类信息，平台现已开通安卓客户端和微信小程序。
+		</div>
+		<div class="project1_left" name="project1_left">
+			<a href="http://www.wyysdsa.cn:1965/" target="_blank"><img class="img3" src="{{URL::asset('images/project/project6.png')}}"/></a>
 		</div>
 	</div>
 </div>
 
-<div style="width:100%;min-height:50px;line-height:50px;text-align:center;color:#333;font-size:15px;">
+<div id="bottom_author" style="width:100%;min-height:50px;line-height:50px;text-align:center;color:#333;font-size:15px;">
 	技术支持：李南希
 </div>
 <!--新改的部分-->
