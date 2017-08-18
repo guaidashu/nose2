04bc37485ce6d1e4666861828a14632159bb06c1
怪大叔丶 <1023767856@qq.com>
2017-08-18 22:22:16 +0800

    ok

--- app/ActModel.php
+++ app/ActModel.php
@@ -15,36 +15,36 @@ class ActModel extends Model
 	  var $row_num;
 	  var $row;
 	  public function dbconnect(){//链接数据库
-		  $this->dblink=mysqli_connect($this->dbLocalhost,$this->dbUser,$this->dbPassword) or die("connect false");
-		  mysqli_select_db($this->dblink,$this->dbDatabase);
-		  mysqli_query($this->dblink,"set names utf8");
+		  $this->dblink=mysql_connect($this->dbLocalhost,$this->dbUser,$this->dbPassword) or die("connect false");
+		  mysql_select_db($this->dbDatabase,$this->dblink);
+		  mysql_query($this->dblink,"set names utf8");
 	  }
 
 	  public function dbresult($q){//获取执行语句并执行
-		  $this->result=mysqli_query($this->dblink,$q);
+		  $this->result=mysql_query($q);
 		  return $this->result;
 	  }
 
 	  public function dbrow($result){//获取数据库里面内容的值
-		  $this->row=mysqli_fetch_assoc($result);
+		  $this->row=mysql_fetch_assoc($result);
 		  return $this->row;
 	  }
 
 	  public function dbrow_num($result){
-		  $this->row_num=mysqli_num_rows($result);
+		  $this->row_num=mysql_num_rows($result);
 		  return $this->row_num;
 	  }
 
 	  public function dbclose(){//关闭数据库
-		  mysqli_close($this->dblink);
+		  mysql_close($this->dblink);
 	  }
 
 	  public function dbfree($result){
-		  mysqli_free_result($result);
+		  mysql_free_result($result);
 	  }
 
 	  public function dbGetId(){
-	  	  $data=mysqli_insert_id($this->dblink);
+	  	  $data=mysql_insert_id($this->dblink);
 	  	  return $data;
 	  }
 }
