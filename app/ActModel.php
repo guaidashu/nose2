8fa33e71ed39ab8c4bf5de7b9a4feb6ceccc742a
怪大叔丶 <1023767856@qq.com>
2017-08-19 09:42:49 +0800

    ok

--- app/ActModel.php
+++ app/ActModel.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class ActModel extends Model
 {
-	protected $connection = 'mysql_center';
+	// protected $connection = 'mysql_center';
     protected $table = "ip";
     //关闭自动维护时间戳(不关闭的话会自动加上数据的时间，维护时间戳)
 	public $timestamps=false;
