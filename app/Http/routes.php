7eaf265db84bf43ac469429d6d1bd451fc98f70f
怪大叔丶 <1023767856@qq.com>
2017-08-20 10:11:58 +0800

    ok

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -37,7 +37,7 @@ Route::post('crn/handle.html', 'CrnController@handle');
 	Route::post('act/searchDorm.html', 'ActController@searchDorm');
 
 	// 获取新生所有信息
-	Route::get('act/getData.html', 'ActController@getNewStudentData');
+	// Route::get('act/getData.html', 'ActController@getNewStudentData');
 
 // 项目展示页面
 Route::get('project', 'ProjectController@index');
