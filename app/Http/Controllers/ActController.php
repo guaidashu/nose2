69df7e7844605769ab2d347bbfd767d5ee0a91e5
怪大叔丶 <1023767856@qq.com>
2017-08-22 23:49:36 +0800

    ok

--- app/Http/Controllers/ActController.php
+++ app/Http/Controllers/ActController.php
@@ -322,7 +322,7 @@ class ActController extends Controller
     	}
     	$zkzh = $_POST['zkzh'];
     	// $cookieUrl = "http://cet.neea.edu.cn/cet/";
-    	$url = "http://cache.neea.edu.cn/Imgs.do?ik=".$zkzh."&t=".rand(1,1000);
+    	$url = "http://cache.neea.edu.cn/Imgs.do?ik=".$zkzh."&t=0.".rand(1,1000);
 		$imgName = "images/verifyGrade.jpg";
 		// 进行cookie的获取
 		$cookieFile = public_path()."/cookie/".md5(date("Y-m-d H:i:s",time())).".cookie";
