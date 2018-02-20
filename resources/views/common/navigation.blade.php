ffe2c3b344110eb3549f22662d3ee4b0b6cbd858
怪大叔丶 <1023767856@qq.com>
2018-02-20 20:00:48 +0800

    ok

--- resources/views/common/navigation.blade.php
+++ resources/views/common/navigation.blade.php
@@ -105,8 +105,8 @@
         <!-- sub-menu start-->
         <ul class="sub-menu">
           <li class="menu-item"><a href="http://sh.wyysdsa.cn/" target="_blank">超级作业</a></li>
-          <li class="menu-item"><a href="http://www.wyysdsa.com/stucontrol/" target="_blank">查寝系统</a></li>
-          <li class="menu-item"><a href="http://www.wyysdsa.cn:1965" target="_blank">川理在线</a></li>
+          <li class="menu-item"><a href="http://stu.wyysdsa.com/" target="_blank">查寝系统</a></li>
+          <li class="menu-item"><a href="https://suse.xsdhy.com/" target="_blank">川理在线</a></li>
         </ul>
         <!-- sub-menu end-->
       </li>
